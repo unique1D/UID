8d161039051329f95c7fa5965f2f0eecfb7f3f50
A Abhiram <abhiram.ampabathina@gmail.com>
2012-05-22 22:08:28 +0530

    The Step1 file, Sorry about the previous one, it was a trial

--- step1.php
+++ step1.php
@@ -1,1 +1,92 @@
-hi
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+	<div id="web">
+		<div id="header">
+		<h4 align="right" style="font-size:10pt">Logged in as : Mr/Mrs Name-of-the-citizen
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="" >Home</a></li>
+				<li><a href="" >Logout</a></li>
+				
+				<li><a href="" >Nearest  Regional Offices</a></li>
+				<li><a href="" >Contact Us</a></li>
+				<li><input type="text" onfocus="focused()" onBlur="blurred()" id="text"></li>
+			</ul>
+		</div>
+		
+	            <br />  <h1 align="center">Welcome to the Unique ID Portal</h1>
+	            <br /><br />
+      <p align=center>    We have a very simple procedure for getting the unique ID.
+
+          This includes a simple form to enroll your present address details.
+
+<br />          Please enter your address details very carefully at first step.
+          You are not allowed to proceed further untill you enroll yourself in the address <br />form.
+
+          After completing the form , you will proceed to stage 2.
+
+          Note that the details you entered are stored, after you click the "submit" button.
+<br />
+          If you want to change the details entered , you should go to your profile to edit Address details.
+
+          Please enter the verification details, and wait<br /> for verification to complete.
+
+          You will receive your reference number at your mail.
+
+          Please, test the reference number by typing it in the test box.<br />
+          Now submit your originals at your nearest agent's area, along with your reference number.
+
+          The agent will check your verification status using<br /> the reference number, and verifies the originals again.
+
+          The agent will also scan your originals and take a picure of yours.
+
+          After taking your<br /> picture, he will give your Unique ID card and update it to your account.
+
+          That is how you get an Unique ID . If your unique ID is lost, you can<br /> claim an duplicate one.
+
+          That facility is also there in the website. The website will give you more support on your Unique ID.
+<br /><br /><br />
+          Kindly fill the form accordingly:
+
+               <br /><br />The details that you provide must be exactly matched to any one of your address proofs
+
+                     <br />      1. ELECTRICITY BILL
+
+                     <br />      2. TELEPHONE BILL(BSNL only)
+
+                     <br />      3. DRIVING LICENSE
+
+                     <br />      4. PASSPORT DETAILS
+
+    <br /><br /><br /><br /><br /><br />
+      <input class="button"type="submit" value="Click here to proceed">
+
+		<div id="">
+		</div>
+		
+	</div>
+ 
+     
+
+
+
+
+	<div id="login">	
+		<input type="button" value="LOGIN" id="lbutton">
+		<div id="loginbox">
+			<table >
+				<tr><td>Username:</td><td><input type="text" id="textb"></td></tr>
+				<tr><td>Password:</td><td><input type="Password" id="textb"></td></tr>
+				<tr><td></td><td><input class="button" type="submit" value="login"></td></tr>
+			</table>
+		</div> 
+	</div>
+</body>
+</html>
